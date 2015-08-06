286d52690015ae653c88a11b81a4a35721994fcc
Mark Bahler <mbahler@cisco.com>
2015-08-06 09:31:23 -0400

    fixed colors in legend

--- plot3.R
+++ plot3.R
@@ -11,6 +11,6 @@ with(epc, {plot(datetime, Sub_metering_1, type = "l",
                 xlab = "", main = "")
            lines(datetime, Sub_metering_2, col = "red")
            lines(datetime, Sub_metering_3, col = "blue")
-           legend("topright", lty = 1, col = c("black", "blue", "red"), 
+           legend("topright", lty = 1, col = c("black", "red", "blue"), 
                   legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))})
 dev.off()
--- plot4.R
+++ plot4.R
@@ -16,7 +16,7 @@ with(epc, {plot(datetime, Global_active_power, type = "l",
                 xlab = "", main = "")
            lines(datetime, Sub_metering_2, col = "red")
            lines(datetime, Sub_metering_3, col = "blue")
-           legend("topright", lty = 1, col = c("black", "blue", "red"), 
+           legend("topright", lty = 1, col = c("black", "red", "blue"), 
                   legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
                   bty = "n", y.intersp = 1)
            plot(datetime, Global_reactive_power, type = "l", main = "")})
